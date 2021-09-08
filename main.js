d61fb291a8c7c430edf6fda0c468c4213a3c9992
Mr_Crafter <80324603+MrCrafterGlitched@users.noreply.github.com>
2021-09-08 15:45:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
+nose_x=""
+nose_y=""
 function preload() {
     
 }
 function setup() {
     canvas=createCanvas(300,300)
     canvas.center()
+    video=createCapture(VIDEO)
+    video.size(300,300)
+    video.hide()
+    posenet=ml5.poseNet(video,model_loaded)
+    posenet.on("pose",getPoses)
 }
 function draw() {
-    
+    image(video,0,0,300,300)
 }
 function TakeSnapshot() {
     save("Filter-ed.png")
+}
+function model_loaded() {
+    console.log("PoseNet is initialiezed")
+}
+function getPoses(Results) {
+    if (Results.length>0) {
+        console.log(Results)
+        nose_x=Results[0].pose.nose.x
+        nose_y=Results[0].pose.nose.y
+        console.log(nose_x,nose_y)
+    }
+    
 }
